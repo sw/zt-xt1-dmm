db348c5601257e7e0a965fa8217e0cb5513e48d1
Stephan Walter <stephan@walter.name>
2026-05-15 16:25:33 +0200

    use DMA interrupt for display update

--- src/display.c
+++ src/display.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#include "at32f403a_407_crm.h"
+#include "at32f403a_407_dma.h"
 #include "at32f403a_407_gpio.h"
+#include "at32f403a_407_misc.h"
+#include "at32f403a_407_spi.h"
 
 #include "src/drivers/display/st7789/lv_st7789.h"
 
-#include "delay.h"
 #include "display.h"
 #include "misc.h"
 
@@ -32,9 +35,10 @@ static void lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size
     gpio_bits_write(GPIOA, GPIO_PINS_15, 1);
 }
 
+static lv_display_t *flush_disp;
 static void lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size)
 {
-    static bool first = true;
+    flush_disp = disp;
 
     gpio_bits_write(GPIOA, GPIO_PINS_15, 0);
 
@@ -47,52 +51,28 @@ static void lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_si
     while (spi_i2s_flag_get(SPI3,SPI_I2S_BF_FLAG)) { }
     gpio_bits_write(GPIOB,GPIO_PINS_4,1);
 
-    #if 0
-    for (size_t i = 0; i < param_size; i++)
-    {
-        spi_i2s_data_transmit(SPI3, param[i]);
-        while (!spi_i2s_flag_get(SPI3,SPI_I2S_TDBE_FLAG)) { }
-    }
-    while (spi_i2s_flag_get(SPI3,SPI_I2S_BF_FLAG)) { }
-    #else
-    dma_init_type dma_init_struct;
     spi_frame_bit_num_set(SPI3, SPI_FRAME_16BIT);
-    dma_reset(DMA2_CHANNEL2);
-    dma_default_para_init(&dma_init_struct);
-    dma_init_struct.buffer_size = param_size / 2;
-    dma_init_struct.direction = DMA_DIR_MEMORY_TO_PERIPHERAL;
-    dma_init_struct.memory_base_addr = (uint32_t)param;
-    dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_HALFWORD;
-    dma_init_struct.memory_inc_enable = TRUE;
-    dma_init_struct.peripheral_base_addr = (uint32_t)&SPI3->dt;
-    dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_HALFWORD;
-    dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
-    dma_init_struct.peripheral_inc_enable = FALSE;
-    dma_init_struct.loop_mode_enable = FALSE;
-    dma_init(DMA2_CHANNEL2, &dma_init_struct);
-    dma_channel_enable(DMA2_CHANNEL2,1);
-    /* TODO: use interrupt */
-    while (!dma_flag_get(DMA2_FDT2_FLAG)) { }
-    dma_reset(DMA2_CHANNEL2);
+    DMA2_CHANNEL2->dtcnt = param_size / 2;
+    DMA2_CHANNEL2->maddr = (uint32_t)param;
+    dma_channel_enable(DMA2_CHANNEL2, TRUE);
+}
+
+void DMA2_Channel2_IRQHandler(void)
+{
+    dma_flag_clear(DMA2_FDT2_FLAG);
+    lv_display_flush_ready(flush_disp);
+    flush_disp = NULL;
+    dma_channel_enable(DMA2_CHANNEL2, FALSE);
+    while (spi_i2s_flag_get(SPI3, SPI_I2S_BF_FLAG)) { }
     spi_frame_bit_num_set(SPI3, SPI_FRAME_8BIT);
-    #endif
-
-    gpio_bits_write(GPIOA, GPIO_PINS_15, 1);
-    lv_display_flush_ready(disp);
-
-    if (first)
-    {
-        /* turn backlight on after first transfer to prevent snow */
-        delay_ms(50);
-        gpio_bits_write(GPIOB, GPIO_PINS_6, 1);
-        first = false;
-    }
+    gpio_bits_write(GPIOA, GPIO_PINS_15, TRUE);
 }
 
 lv_display_t *display_init(void)
 {
     spi_init_type spi_init_struct;
 
+    gpio_bits_write(GPIOB, GPIO_PINS_15, TRUE);
     gpio_init_simple(GPIOB, GPIO_PINS_15, GPIO_MODE_OUTPUT, GPIO_PULL_NONE);
     gpio_init_simple(GPIOB, GPIO_PINS_4, GPIO_MODE_OUTPUT, GPIO_PULL_NONE);
     gpio_init_simple(GPIOA, GPIO_PINS_15, GPIO_MODE_OUTPUT, GPIO_PULL_NONE);
@@ -113,9 +93,19 @@ lv_display_t *display_init(void)
     spi_i2s_dma_transmitter_enable(SPI3, TRUE);
     spi_enable(SPI3, 1);
 
-    gpio_bits_write(GPIOB, GPIO_PINS_15, FALSE);
-    delay_ms(1);
-    gpio_bits_write(GPIOB, GPIO_PINS_15, TRUE);
+    dma_init_type dma_init_struct;
+    dma_default_para_init(&dma_init_struct);
+    dma_init_struct.direction = DMA_DIR_MEMORY_TO_PERIPHERAL;
+    dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_HALFWORD;
+    dma_init_struct.memory_inc_enable = TRUE;
+    dma_init_struct.peripheral_base_addr = (uint32_t)&SPI3->dt;
+    dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_HALFWORD;
+    dma_init_struct.priority = DMA_PRIORITY_MEDIUM;
+    dma_init_struct.peripheral_inc_enable = FALSE;
+    dma_init_struct.loop_mode_enable = FALSE;
+    dma_init(DMA2_CHANNEL2, &dma_init_struct);
+    dma_interrupt_enable(DMA2_CHANNEL2, DMA_FDT_INT, TRUE);
+    nvic_irq_enable(DMA2_Channel2_IRQn, 8, 0);
 
     #define LCD_H_RES       240
     #define LCD_V_RES       320
@@ -132,7 +122,7 @@ lv_display_t *display_init(void)
 
     /* backlight */
     /* TODO: PWM for brightness */
-    gpio_bits_write(GPIOB, GPIO_PINS_6, 0);
+    gpio_bits_write(GPIOB, GPIO_PINS_6, TRUE);
     gpio_init_simple(GPIOB, GPIO_PINS_6, GPIO_MODE_OUTPUT, GPIO_PULL_NONE);
 
     return lcd_disp;
